c0566856c139adf3b708465b8865872c3a327871
Andrew Craven <andrew@epworth-consulting.co.uk>
2018-11-02 17:07:37 +0000

    Split away from grouchy.httpapi.server.testing

---
+++ src/Grouchy.HttpApi.Client.Testing.Tests/handle_request_with_stub_http_api.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+
+#if NET451
+   using Microsoft.Owin;
+#else
+   using Microsoft.AspNetCore.Http;
+#endif
+
+namespace Grouchy.HttpApi.Client.Testing.Tests
+{
+   public class handle_request_with_stub_http_api
+   {
+      [Test]
+      public async Task simple_get_request()
+      {
+         using (var webApi = new PingHttpApi())
+         using (var httpClient = new HttpClient { BaseAddress = webApi.BaseUri })
+         {
+            var response = await httpClient.GetAsync("/ping");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            content.ShouldBe("pong");
+         }
+      }
+
+      private class PingHttpApi : StubHttpApi
+      {
+#if NET451
+         protected override async Task Handler(IOwinContext context)
+#else
+         protected override async Task Handler(HttpContext context)
+#endif
+         {
+            if (context.Request.Method == "GET" && context.Request.Path.ToString() == "/ping")
+            {
+               context.Response.StatusCode = (int)HttpStatusCode.OK;
+               await context.Response.WriteAsync("pong");
+            }
+            else
+            {
+               await base.Handler(context);
+            }
+         }
+      }
+   }
+}
